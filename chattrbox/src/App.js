af847cbdd8044b72c55ec11a98442c5861daf652
Ian Harris <ianmharris93@gmail.com>
2021-10-18 22:15:42 -0700

    added functioning message/user message forms and a message form

--- chattrbox/src/App.js
+++ chattrbox/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import "./App.js"; 
-import { ChatEngine } from 'react-chat-engine'
+import "./App.css"; 
+import { ChatEngine} from 'react-chat-engine'
+import Feed from "./components/Feed"
 
 
 export function App() {
   return (
     <ChatEngine
-      height = "100vh"
-      projectID= "14530bfe-8c29-4e09-8c9a-dc073116e156"
-      userName="Ian"
-      userSecret="bananaboy10"
+    height = "100vh"
+    projectID= "14530bfe-8c29-4e09-8c9a-dc073116e156"
+    userName="Ian"
+    userSecret="bananaboy10"
+    renderChatFeed = {(props) => <Feed {... props} />}
+    
     />
-  )
+    
+    
+    )
 }
 
 export default App;
---
+++ chattrbox/src/components/Feed.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import GuestMessage from "./GuestMessage";
+import Message from "./Message";
+import MyMessage from "./MyMessage";
+
+const Feed = (props) => {
+    const { chats, activeChat, userName, messages } = props
+
+    // checking to see if chats exist, then looking for specific active chat
+    const chat = chats && chats[activeChat]
+
+    // console.log(chat, userName, messages)
+    // function for generatiing messages
+    const loadMessage = () => {
+        const messageKeys = Object.keys(messages)
+
+        return messageKeys.map((key, index) => {
+            const message = messages[key]
+            //  if there are messages,  find the last message
+            const finalMessage = index === 0 ? null : messageKeys[index - 1]
+            const ifMyMessage = userName === message.sender.username;
+
+
+            return (
+                //  mapping through, need index
+                <div key={`message_${index}`} style={{ width: '100%' }}>
+                    <div className="message-block">
+                        {/* if message is mine then render my mesage */}
+                        {/* if not my message, render guest message */}
+                        {
+                            ifMyMessage
+                                ? <MyMessage message = {message} />
+                                : <GuestMessage message ={message} lastMessage = {messages[finalMessage]} />
+                        }
+
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: ifMyMessage ? '18px' : '0px', marginLeft: ifMyMessage ? '18px' : '65px' }}>
+                        
+                    </div>
+
+                </div>
+            )
+        })
+
+    }
+    
+    if (!chat) {
+        return "Loading, please wait..."
+    }
+    return (
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat.title}</div>
+                <div className="chat-subtitle">
+                    {/* map over all people, find person, set subtitle to username */}
+                    {chat.people.map((person) => `${person.person.username}`)}
+                </div>
+
+            </div>
+            {loadMessage()}
+            <div style={{ height: "100px" }} />
+            <div className = "message-form-container">
+                <Message {... props} chatId={activeChat}/>
+            </div>
+        </div>
+    )
+
+}
+
+export default Feed;
